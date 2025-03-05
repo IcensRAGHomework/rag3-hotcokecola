68029f1232a3fb103fca2436f03edb2a4eed1d9d
hsiuting_yang <hsiuting_yang@asus.com>
2025-03-05 10:17:55 +0800

    HW3 test2

--- student_assignment.py
+++ student_assignment.py
@@ -1,12 +1,10 @@
 import datetime
 import chromadb
 import traceback
+import csv
 
 from chromadb.utils import embedding_functions
 from model_configurations import get_model_configuration
-
-
-import csv
 from datetime import datetime
 
 
@@ -15,13 +13,25 @@ gpt_emb_config = get_model_configuration(gpt_emb_version)
 
 dbpath = "./"
 
-def generate_hw01():   
+def generate_hw01(): 
+    # Create embedding function
+    openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+        api_key = gpt_emb_config['api_key'],
+        api_base = gpt_emb_config['api_base'],
+        api_type = gpt_emb_config['openai_type'],
+        api_version = gpt_emb_config['api_version'],
+        deployment_id = gpt_emb_config['deployment_name']
+    )
+
     # Initialize ChromaDB client
     client = chromadb.Client()
     
     # Create a new collection
-    collection = client.create_collection(name="TRAVEL", metadata={
-        "hnsw:space": "cosine"})
+    collection = client.create_collection(
+        name="TRAVEL", 
+        metadata={"hnsw:space": "cosine"},
+        embedding_function = openai_ef
+    )
 
     # Function to parse dates and calculate seconds from the epoch start date
     def calculate_unix_timestamp(date_str):
@@ -35,13 +45,13 @@ def generate_hw01():
 
     with open(csv_file_path, mode='r', encoding='utf-8-sig') as file:
         csv_reader = csv.DictReader(file)
-        headers = csv_reader.fieldnames
+        #headers = csv_reader.fieldnames
         #print("CSV Headers:", headers)  # Print headers to verify   
 
         for row in csv_reader:
             # Add metadata to the collection
             collection.add(
-                ids=row['ID'],
+                ids=str(row['ID']),
                 documents=row['HostWords'],
                 metadatas={
                     'name': row['Name'],
